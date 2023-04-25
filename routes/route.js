4968437a35525e963bf1779c4b37a4b38af91375
Pal Bijewar <pal.bijewar@gmail.com>
2023-04-25 12:53:24 +0530

    assignment for post api created

--- routes/route.js
+++ routes/route.js
@@ -5,51 +5,66 @@ const router = express.Router();
 const express = require('express');
 const app = express();
 
+// Q3. post api problem set
 
 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
 
-// Q1. write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
 
-
-app.get('/sol1', function (req, res){
-    let arr= [1,2,3,5,6,7];
-    let sum =(7*(7+1))/2;  
-    let add = 0;
-    for (let i=0; i <arr.length; i++) {
-        add +=arr[i];
+   router.post('/players', function (req, res) {
+    //LOGIC WILL COME HERE
+    let body=req.body;
+    let x=[];
+    for(let i=0;i<players.length;i++)
+    {
+        curr=players[i];
+        if(Object.values(curr).includes(curr.name)!=body.name)
+        {
+            x.push(curr.name);
+        }
     }
-    let missingNumber = sum - add;
-    res.send(  { data: missingNumber  }  );
+    
+    if(x.includes(body.name)!=true)
+    {
+        players.push(body);
+    }
+    else
+    {
+        res.send("This person already exists");
+    }
+    res.send(  { data: players , status: true }  );
+})
 
-
-});
-
-
-
-
-// Q2. write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
-
-
-app.get("/sol2", function (req, res) {
-  let arr = [33, 34, 35, 37, 38];
-  let missingNumber;
-
-
-  const firstNumber = arr[0];
-  const lastNumber = arr[arr.length - 1];
-  const sequenceLength = arr.length + 1;
-
-
-  const expectedSum = (sequenceLength * (firstNumber + lastNumber)) / 2;
-  const actualSum = arr.reduce((sum, num) => sum + num);
-
-
-  missingNumber = expectedSum - actualSum;
-
-
-  res.send({ data: missingNumber });
-});
-
+module.exports = router;
 
 const port = 3000;
 app.listen(port, () => {
